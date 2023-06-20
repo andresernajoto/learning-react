affdaa13b521de751ce2801728d72317e927032a
André Sernajoto <andre.sernajoto@gmail.com>
2023-06-20 00:00:13 -0300

    first steps
    Adding styles and a little of content

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet, Platform, StatusBar } from 'react-native'
 
-export default function App() {
-  const message = 'Hellow';
-  
+import { Feather } from '@expo/vector-icons'
+
+const App = () => {
   return (
-    <View style={{ height: 100, width: 100, backgroundColor: 'orange' }}>
-      <Text>{ message }</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black" />
+
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}> | Low: 6</Text>
+        </View>
+      </View>
+
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>It's sunny</Text>
+        <Text style={styles.message}>It's perfect t-shirt weather</Text>
+      </View>
+      
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'pink'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
-});
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    color: 'black',
+    fontSize: 30
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row'
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    paddingLeft: 25,
+    marginBottom: 40
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
+  }
+})
+
+export default App

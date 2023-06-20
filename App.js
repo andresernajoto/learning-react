0ffdad591fb72bc714454fbf00d62ce37155f428
André Sernajoto <andre.sernajoto@gmail.com>
2023-06-19 22:41:07 -0300

    creating project

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const message = 'Hellow';
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ height: 100, width: 100, backgroundColor: 'orange' }}>
+      <Text>{ message }</Text>
     </View>
   );
 }

33b28e2b107d2c2d728d45fd08db4378a1cea3ad
André Sernajoto <andre.sernajoto@gmail.com>
2023-06-23 00:05:02 -0300

    starting to implement navigation
    Using react native navigation to navigate between the app screens

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import React from "react"
-import { View, StyleSheet, StatusBar } from "react-native"
+import { StyleSheet, StatusBar, Platform } from "react-native"
 
 import CurrentWeather from "./src/screens/CurrentWeather"
 import UpcomingWeather from "./src/screens/UpcomingWeather"
 import City from "./src/screens/City"
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+
+const BottomTab = createBottomTabNavigator()
+const TopTap = createMaterialTopTabNavigator()
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-    </View>
+    <NavigationContainer>
+      {Platform.OS === 'android' ? (
+        <TopTap.Navigator  style={{ marginTop: StatusBar.currentHeight || 0 }}>
+          <TopTap.Screen name={'Current'} component={CurrentWeather} />
+          <TopTap.Screen name={'Upcoming'} component={UpcomingWeather} />
+          <TopTap.Screen name={'City'} component={City} />
+        </TopTap.Navigator>
+      ) : (
+        <BottomTab.Navigator>
+          <BottomTab.Screen name={'Current'} component={CurrentWeather} />
+          <BottomTab.Screen name={'Upcoming'} component={UpcomingWeather} />
+          <BottomTab.Screen name={'City'} component={City} />
+        </BottomTab.Navigator>
+      )}     
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0
-  }
-})
 
 export default App
